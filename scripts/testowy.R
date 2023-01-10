a7c54db1fd846ca188df47a769e1a80510771aa4
Szymon <62554479+Fioletuwa@users.noreply.github.com>
2023-01-10 23:39:10 +0100

    Pierwsza pełna tabela - statystyki graczy

---
+++ scripts/testowy.R
@@ -0,0 +1,44 @@
+library(rvest)
+library(tidyverse)
+
+url1 <- "https://lol.fandom.com/wiki/2022_Season_World_Championship/Main_Event/Player_Statistics"
+page_sg <- read_html(url1)
+
+statystyki_graczy <- html_table(page_sg)
+
+player_stats_tibble <- statystyki_graczy[[5]]
+
+clean_player_stats <- player_stats_tibble[-c(1:3),-c(1,21:27)]
+names(clean_player_stats) <- clean_player_stats[1,]
+
+clean_player_stats <- clean_player_stats[-1,]
+
+champs_all_page <- page_sg %>%
+  html_elements("span.sprite.champion-sprite") %>%
+  html_attr("title")
+
+champs_player_table <- champs_all_page[-c(1:10)]
+
+players1to15 <- champs_player_table[1:45]
+
+player16 <- champs_player_table[46]
+
+players17to21 <- champs_player_table[47:61]
+
+player22 <- champs_player_table[62]
+
+players23to82 <- champs_player_table[63:242]
+
+vec <- c(players1to15, player16, NA, NA, players17to21, player22, NA, NA, players23to82)
+
+champs_most_picked <- as_tibble(matrix(data = vec, ncol = 3, byrow = T), .name_repair = "unique")
+
+names(champs_most_picked) <- c("Most_picked1","Most_picked2","Most_picked3")
+
+player_with_champs <- cbind(clean_player_stats, champs_most_picked)
+
+names(player_with_champs)[1] <- "Player"
+
+Team <- c(rep("100 Thieves", times = 5), rep("Cloud9", times = 5), rep("CTBC Flying Oyster", times = 6), rep("DRX", times = 6), rep("DWG KIA", times = 5), rep("EDward Gaming", times = 5), rep("Evil Geniuses.NA", times = 5), rep("Fnatic", times = 5), rep("G2 Esports", times = 5), rep("GAM Esports", times = 5), rep("Gen.G", times = 5), rep("JD Gaming", times = 5), rep("Rogue", times = 5), rep("Royal Never Give Up", times = 5), rep("T1", times = 5), rep("Top Esports", times = 5))
+
+player_with_champs <- cbind(Team, player_with_champs)
